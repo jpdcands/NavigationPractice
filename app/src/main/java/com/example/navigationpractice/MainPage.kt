ca373ea0b8c5f35a903c2c4f3341238f960647d0
jpdcands <jpdcands211@gmail.com>
2024-01-31 20:26:50 -0500

    Last commit 1/31/24

--- app/src/main/java/com/example/navigationpractice/MainPage.kt
+++ app/src/main/java/com/example/navigationpractice/MainPage.kt
@@ -2,30 +2,41 @@ package com.example.navigationpractice
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
 
 @Composable
 fun MainPage(navController: NavController) {
-    Scaffold { paddingValues ->
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Main Page")
-            Button(onClick = { navController.navigate("exercise") }) {
-                Text(text = "Go to Exercise Page")
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), // You can adjust this padding as needed
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Main Page",
+            style = TextStyle(fontSize = 36.sp, fontWeight = FontWeight.Bold)
+        )
+        Spacer(modifier = Modifier.height(16.dp)) // Optional: Spacer for some space between Text and Button
+        Button(onClick = { navController.navigate("exercise") }) {
+            Text(text = "Go to Exercise Page")
         }
     }
 }
