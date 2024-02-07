bd94e8d2c55e43113b90c8d2ca1046a17170a8b2
jpdcands <jpdcands211@gmail.com>
2024-02-07 06:10:28 -0500

    Start 2/7/24

--- app/src/main/java/com/example/navigationpractice/MainActivity.kt
+++ app/src/main/java/com/example/navigationpractice/MainActivity.kt
@@ -21,10 +21,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MyApp()
-            }
+            MyApp()
         }
     }
+}S
 
 @Composable
 fun MyApp() {
